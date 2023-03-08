20c528221205efe20299de26004c5a16e207e514
Deabmaister68 <514109@vistacollege.nl>
2023-03-08 12:23:58 +0100

    test

--- back_end/index.php
+++ back_end/index.php
@@ -1,13 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$servername = "localhost";
+$username = "john";
+$password = "";
+$dbname = "mode";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
+
+<?php
+$sql = "SELECT * FROM posts";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "Post ID: " . $row["post_id"] . "<br>";
+        echo "Content: " . $row["content"] . "<br>";
+        echo "Likes: " . $row["likes"] . "<br>";
+        // ...and so on
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <title>My Form</title>
 </head>
 <body>
+    <form>
+        <label for="postContent">Post content:</label>
+        <input type="text" id="postContent" name="postContent"><br><br>
+        <button type="button" onclick="submitForm()">Submit</button>
+    </form>
     
+    <div id="result"></div>
+    
+    <script>
+        function submitForm() {
+            var content = document.getElementById("postContent").value;
+            
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("result").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("POST", "submit_post.php", true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send("content=" + content);
+        }
+    </script>
 </body>
 </html>
+
+
+
+
