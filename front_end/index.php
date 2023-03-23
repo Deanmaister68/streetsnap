8ec8139db44a1f741f049726d67451ca4fe483c7
Deabmaister68 <514109@vistacollege.nl>
2023-03-23 16:23:14 +0100

    last

---
+++ front_end/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Styles.css">
+</head>
+    <header>
+        
+          <div > <img src="./imges/logo.png" alt="" id="fotologo"></div>
+            <div id="navigation"> 
+                  <a href="">Home</a>
+                  <a href="">Create</a>
+              </div>
+
+                <div id="logo">
+                  <h1>SNAP FASHION</h1> 
+                </div>
+
+                <form class="search_pool">
+                  <input  type="search" name="q" placeholder="   Zoek..." />
+                  <button type="submit">Zoek</button>
+                </form>
+
+                <div id="fotoprofile1">
+                  <a href="#">
+                    <img src="./imges/Schermafbeelding_2023-03-09_103238-removebg-preview.png" alt="" id="fotoprofile">
+                  </a> 
+                </div>
+              
+    </header>
+<body>
+<div id="box">
+  
+<div class="post1">
+  <img src="streetsnap1.png" alt=""  width="240" height="280" border-radius="10%">
+  <p> <h1>alwaizsteampunk</h1> Just in case his handshake and word of honor isn't enough...A little lead always makes a big difference.😎💯</p>
+</div>
+
+<div class="post1">
+  <img src="./imges/foto1.png" alt=""  width="240" height="280" border-radius="10%">
+  <p> <h1>alwaizsteampunk</h1> Just in case his handshake and word of honor isn't enough...A little lead always makes a big difference.😎💯</p>
+</div>
+
+<div class="post1">
+  <img src="./imges/foto1.png" alt=""  width="240" height="280" border-radius="10%">
+  <p> <h1>alwaizsteampunk</h1> Just in case his handshake and word of honor isn't enough...A little lead always makes a big difference.😎💯</p>
+</div>
+
+<div class="post1">
+  <img src="./imges/foto1.png" alt=""  width="240" height="280" border-radius="10%">
+  <p> <h1>alwaizsteampunk</h1> Just in case his handshake and word of honor isn't enough...A little lead always makes a big difference.😎💯</p>
+</div>
+</div>
+
+
+<div id="box1">
+
+<div class="post2">
+  <img src="./imges/foto1.png" alt=""  width="240" height="280" border-radius="10%">
+  <p> <h1>alwaizsteampunk</h1> Just in case his handshake and word of honor isn't enough...A little lead always makes a big difference.💯</p>
+</div>
+
+<div class="post2">
+  <img src="./imges/foto1.png" alt=""  width="240" height="280" border-radius="10%">
+  <p> <h1>alwaizsteampunk</h1> Just in case his handshake and word of honor isn't enough...A little lead always makes a big difference.💯</p>
+</div>
+<div class="post2">
+  <img src="./imges/foto1.png" alt=""  width="240" height="280" border-radius="10%">
+  <p> <h1>alwaizsteampunk</h1> Just in case his handshake and word of honor isn't enough...A little lead always makes a big difference.💯</p>
+</div>
+
+<div class="post2">
+  <img src="./imges/foto1.png" alt=""  width="240" height="280" border-radius="10%">
+  <p> <h1>alwaizsteampunk</h1> Just in case his handshake and word of honor isn't enough...A little lead always makes a big difference.💯</p>
+</div>
+
+
+</div>
+
+
+</body>
+
+
+</html>
